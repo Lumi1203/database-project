32ee7ce1bfe29a8f924487c8b7cab035c66da493
Olumide Olumoyin <engrlumynem@gmail.com>
2025-09-20 14:40:20 +0100

    connection string corrected

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
-connection_string = "postgresql://postgres:5869@localhost/accident_report_platform"
+connection_string = "postgresql://postgres:5869@localhost:5432/accident_report_platform"
 
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', connection_string)
